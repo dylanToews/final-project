8755f474325d4a885ef3c10a081b014dc964a857
dylanToews <dylan.toews@yahoo.ca>
2023-01-06 08:30:59 -0700

    initial push. Setup Express server with dummy data for db

---
+++ backend/server/routes/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const users = require('../db/queries/users');
+
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+  users.getAllUsers().then(data => {
+    console.log(data);
+    res.json({users: data});
+  })
+});
+
+module.exports = router;
