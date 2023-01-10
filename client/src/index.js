998fb2506fccd069b8beb9628ab6f5795c944b0c
dylanToews <dylan.toews@yahoo.ca>
2023-01-10 08:05:06 -0700

    latest commit

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ContextAlarm from './components/context/AlarmProvider';
+// import ContextAlarm from './components/context/AlarmProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ContextAlarm>
+    {/* <ContextAlarm> */}
     <App />
-    </ContextAlarm>
+    {/* </ContextAlarm> */}
   </React.StrictMode>
 );
 
