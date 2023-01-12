b705f6d6f878fe77b417d2af05100d7f16d03c57
Cheever Esler <ames.esler@gmail.com>
2023-01-12 13:54:51 -0400

    set up default sounds, modify db seeds

--- server/db/seeds/02_sounds.sql
+++ server/db/seeds/02_sounds.sql
@@ -1,23 +1,19 @@
 -- seeds/02_sounds.sql
 
--- Seed Rick
-INSERT INTO sounds (user_id, title, url) VALUES (1, 'Rick 1', 'exampleurl01.notvalid.example');
-INSERT INTO sounds (user_id, title, url) VALUES (1, 'Rick 2', 'exampleurl02.notvalid.example');
-INSERT INTO sounds (user_id, title, url) VALUES (1, 'Rick 3', 'exampleurl03.notvalid.example');
-INSERT INTO sounds (user_id, title, url) VALUES (1, 'Rick 4', 'exampleurl04.notvalid.example');
+-- Seed Dylan
+INSERT INTO sounds (user_id, title, url) VALUES (1, 'Soft Wakeup', 'soft_wakeup.ogg');
+INSERT INTO sounds (user_id, title, url) VALUES (1, 'Rough Wakeup', 'rough_wakeup.ogg');
+INSERT INTO sounds (user_id, title, url) VALUES (1, 'WAKE UP!', 'big_wakeup.ogg');
 
--- Seed Lisa
-INSERT INTO sounds (user_id, title, url) VALUES (2, 'Lisa 1', 'exampleurl05.notvalid.example');
-INSERT INTO sounds (user_id, title, url) VALUES (2, 'Lisa 2', 'exampleurl06.notvalid.example');
-INSERT INTO sounds (user_id, title, url) VALUES (2, 'Lisa 3', 'exampleurl07.notvalid.example');
-INSERT INTO sounds (user_id, title, url) VALUES (2, 'Lisa 4', 'exampleurl08.notvalid.example');
+-- Seed Ryan
+INSERT INTO sounds (user_id, title, url) VALUES (2, 'Soft Wakeup', 'soft_wakeup.ogg');
+INSERT INTO sounds (user_id, title, url) VALUES (2, 'Rough Wakeup', 'rough_wakeup.ogg');
+INSERT INTO sounds (user_id, title, url) VALUES (2, 'WAKE UP!', 'big_wakeup.ogg');
 
--- Seed Link
-INSERT INTO sounds (user_id, title, url) VALUES (3, 'Link 1', 'exampleurl09.notvalid.example');
-INSERT INTO sounds (user_id, title, url) VALUES (3, 'Link 2', 'exampleurl10.notvalid.example');
-INSERT INTO sounds (user_id, title, url) VALUES (3, 'Link 3', 'exampleurl11.notvalid.example');
+-- Seed Cheever
+INSERT INTO sounds (user_id, title, url) VALUES (3, 'Soft Wakeup', 'soft_wakeup.ogg');
+INSERT INTO sounds (user_id, title, url) VALUES (3, 'Rough Wakeup', 'rough_wakeup.ogg');
+INSERT INTO sounds (user_id, title, url) VALUES (3, 'WAKE UP!', 'big_wakeup.ogg');
 
--- Seed Simon
-INSERT INTO sounds (user_id, title, url) VALUES (4, 'Simon 1', 'exampleurl12.notvalid.example');
-
--- Seed Mario (no sounds)
+-- Seed Guest
+INSERT INTO sounds (user_id, title, url) VALUES (4, 'Rough Wakeup', 'rough_wakeup.ogg');
