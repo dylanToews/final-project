dea1455423aec480c67c3e6d46ec32d816a48c26
Cheever Esler <ames.esler@gmail.com>
2023-01-11 23:26:27 -0400

    add basic SoundListItem audio controls

--- client/src/components/Sounds/SoundListItem.jsx
+++ client/src/components/Sounds/SoundListItem.jsx
@@ -3,7 +3,8 @@ export default function SoundListItem(props) {
 
   return (
     <li className="SoundListItem">
-      <p>This is a sound list item</p>
+      <p>Sound Title</p><br />
+      <audio controls controlsList="nodownload" src="http://localhost:8080/audio/1673469843174.ogg"></audio>
     </li>
   )
 }
