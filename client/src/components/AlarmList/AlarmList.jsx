fb1bd60dfc1647c8f23dada9e78e045bd8274949
Cheever Esler <ames.esler@gmail.com>
2023-01-14 15:45:18 -0400

    start on arranging alarms, removed some console.logs from auth provider

--- client/src/components/AlarmList/AlarmList.jsx
+++ client/src/components/AlarmList/AlarmList.jsx
@@ -5,6 +5,15 @@ import AlarmListItem from "./AlarmListItem";
 export default function AlarmList(props) {
   const { alarmItems } = useContext(AlarmContext);
 
+  // relevant keys from alarmItem: 
+  // am_pm: string, "AM" or "PM"
+  // hour : string, 01-12
+  // minutes: string, 00-59
+
+  // const arrangedAlarmItems = alarmItems.sort(
+  //   (a1, a2) => (a1.)
+  // )
+
   const parsedAlarmItems = alarmItems.map((alarmItem) => (
     <AlarmListItem key={alarmItem.id} {... alarmItem} />
   ))
