9e4413855d254487709fe9b6f19237bc9fff6c02
dylanToews <dylan.toews@yahoo.ca>
2023-01-14 11:55:40 -0700

    rough positioning of card items within rows and columns

--- client/src/components/AlarmList/AlarmListItem.jsx
+++ client/src/components/AlarmList/AlarmListItem.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import axios from "axios";
 import { AlarmContext } from "../context/AlarmProvider";
-import { Card, Button, Container, Row, Col, Accordion } from "react-bootstrap";
+import { Card, Button, Container, Row, Col, Accordion, Dropdown } from "react-bootstrap";
 export default function AlarmListItem(props) {
   const { id, user, hour, minutes, am_pm, contact_name, sound_name, active } =
     props;
@@ -40,37 +40,59 @@ export default function AlarmListItem(props) {
     <Container fluid>
       <Card className="mt-2 shadow-lg">
         <Card.Body>
-          <Card.Title className="display-2">{`${hour}:${minutes} ${am_pm}`}</Card.Title>
-          <Accordion>
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>Alarm Details</Accordion.Header>
-              <Accordion.Body>
-                <Card.Text>Contacts: {contact_name}</Card.Text>
-                <Card.Text>Sound: {sound_name}</Card.Text>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-      <div className="d-flex justify-content-around">
-          <Button
-            variant="outline-secondary"
-            onClick={() => onToggle(id)}
-            className="d-flex justify-content-around"
-          >
-            {active && "ON"}
-            {!active && "OFF"}
-          </Button>
-          <Button
-            variant="outline-secondary"
-            onClick={() => removeAlarm(id)}
-            className="d-flex justify-content-around"
-          >
-            Delete
-          </Button>
-          </div>
+          <Row>
+            <Col>
+              <Card.Title className="display-10">{`${hour}:${minutes} ${am_pm}`}</Card.Title>
+            </Col>
+
+            <Col>
+              <Button variant="outline-secondary" onClick={() => onToggle(id)}>
+                {active && "ON"}
+                {!active && "OFF"}
+              </Button>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Details
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item>Contacts: {contact_name}</Dropdown.Item>
+                  <Dropdown.Item>
+                  Sound: {sound_name}
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+            <Col>
+              <Button
+                variant="outline-secondary"
+                onClick={() => removeAlarm(id)}
+              >
+                Delete
+              </Button>
+            </Col>
+          </Row>
         </Card.Body>
       </Card>
     </Container>
   );
+}
+
+{
+  /* <Accordion>
+<Accordion.Item eventKey="0">
+  <Accordion.Header>Alarm Details</Accordion.Header>
+  <Accordion.Body>
+    <Card.Text>Contacts: {contact_name}</Card.Text>
+    <Card.Text>Sound: {sound_name}</Card.Text>
+  </Accordion.Body>
+</Accordion.Item>
+</Accordion> */
 }
 
 {
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import "../About.css";
+// import "../About.css";
 
+import { Card, Button, Container, Row, Col, Accordion } from "react-bootstrap";
 function About() {
 
 
 
     return (
-        <div className="App">
-            <br/>
-            <br/>
-            <h1>LEARN</h1>
-            <br/>
-            <h2>Welcome to Startle, the App that does... something.</h2>
-            <br/>
-            <h3>We'll add to this later</h3>
-            <p>In the meantime, this page will be used to test styling</p>
-        </div>
+<Container fluid>
+  <Row>
+    <Col >Time</Col>
+    <Col >Active?</Col>
+  </Row>
+  <Row>
+    <Col>Details</Col>
+    <Col>Delete</Col>
+  </Row>
+</Container>
     );
 }
 
